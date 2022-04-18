b33fdbd992208d68d487ab203bfa398c2bf69eeb
Hasan <hasan.bakrci@gmail.com>
2022-04-18 15:26:23 +0300

    empty commit.

---
+++ sub-service/Program.cs
@@ -0,0 +1,7 @@
+using sub_service;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services => { services.AddHostedService<Worker>(); })
+    .Build();
+
+await host.RunAsync();
